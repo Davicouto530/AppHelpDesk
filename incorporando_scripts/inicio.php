af9064bb37950c6637d5b14c538dfdb0e0f7627d
Davi do Couto <162334727+Davicouto530@users.noreply.github.com>
2025-09-09 19:06:53 -0300

    Incorporando scripts com include, include_once, require e require_once.

---
+++ incorporando_scripts/inicio.php
@@ -0,0 +1,14 @@
+<?php
+    include("menu.php"); // Se der erro gera um: warning
+
+    // require("menu.php"); // Se der erro gera um: fatal error
+?>
+conteúdo da página (inicio)
+
+<br>
+
+<?php
+    // include_once("menu.php"); // O "include_once" só permite incluir uma vez
+
+    require_once("menu.php"); // O "require_once" só permite incluir uma vez
+?>
